daa7875732e294ba2cce96a0f1d90fdddef86708
hejunwei <hejunwei@bluestoneapp.com>
2018-02-23 22:19:27 +0800

    完善readme

---
+++ digraph.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hejunwei on 23/02/2018.
+//
+#include "digraph.h"
+#include <iostream>
+
+using namespace std;
+
+template<int graph_size>
+void Digraph::read() {
+    cout << "read";
+}
--- digraph.h
+++ digraph.h
@@ -7,6 +7,7 @@
 
 #include <list>
 #include <queue>
+#include "digraph.cpp"
 
 typedef int Vertex;
 
@@ -19,63 +20,63 @@ public:
 
     void write();
 
-    void depth_sort(list <Vertex> &topological_order) {
-        bool visited[graph_size];
-        Vertex v;
-        for (int v = 0; v < this->count; ++v) visited[v] = false;
-        topological_order.clear();
-        for (int v = 0; v < this->count; ++v) {
-            if (!visited[v]) {
-                recursive_depth_sort(v, visited, topological_order);
-            }
-        }
-    }
+//    void depth_sort(list <Vertex> &topological_order) {
+//        bool visited[graph_size];
+//        Vertex v;
+//        for (int v = 0; v < this->count; ++v) visited[v] = false;
+//        topological_order.clear();
+//        for (int v = 0; v < this->count; ++v) {
+//            if (!visited[v]) {
+//                recursive_depth_sort(v, visited, topological_order);
+//            }
+//        }
+//    }
 
-    void breadth_sort(list <Vertex> &topological_order) {
-        topological_order.clear();
-        Vertex v, w;
-        int predecessor_count[graph_size];
-        for (int v = 0; v < this->count; ++v) predecessor_count[v] = 0;
-        for (int v = 0; v < this->count; ++v) {
-            for (int i = 0; i < this->neighbors[v].size(); ++i) {
-                neighbors[v].retrieve(i, w);
-                predecessor_count[w]++;
-            }
-            queue<int> ready_to_process;
-            for (int v = 0; v < this->count; ++v) {
-                if (predecessor_count[v] == 0)
-                    ready_to_process.push(v);
-            }
-            while(!ready_to_process.empty()) {
-                ready_to_process.retrieve(v);
-                topological_order.insert(topological_order.size(), v);
-                for (int i = 0; i < this->neighbors[v].size(); ++i) {
-                    neighbors[v].retrieve(j, w);
-                    predecessor_count[w]--;
-                    if (predecessor_count[w] == 0)
-                        ready_to_process.push(w);
-                }
-                ready_to_process.serve();
-            }
-        }
-    }
+//    void breadth_sort(list <Vertex> &topological_order) {
+//        topological_order.clear();
+//        Vertex v, w;
+//        int predecessor_count[graph_size];
+//        for (int v = 0; v < this->count; ++v) predecessor_count[v] = 0;
+//        for (int v = 0; v < this->count; ++v) {
+//            for (int i = 0; i < this->neighbors[v].size(); ++i) {
+//                neighbors[v].retrieve(i, w);
+//                predecessor_count[w]++;
+//            }
+//            queue<int> ready_to_process;
+//            for (int v = 0; v < this->count; ++v) {
+//                if (predecessor_count[v] == 0)
+//                    ready_to_process.push(v);
+//            }
+//            while(!ready_to_process.empty()) {
+//                ready_to_process.retrieve(v);
+//                topological_order.insert(topological_order.size(), v);
+//                for (int i = 0; i < this->neighbors[v].size(); ++i) {
+//                    neighbors[v].retrieve(j, w);
+//                    predecessor_count[w]--;
+//                    if (predecessor_count[w] == 0)
+//                        ready_to_process.push(w);
+//                }
+//                ready_to_process.serve();
+//            }
+//        }
+//    }
 
 private:
     int count;
-    list <Vertex> neighbors[graph_size];
+//    list <Vertex> neighbors[graph_size];
 
-    void recursive_depth_sort(Vertex v, bool visited[], list <Vertex> &topological_order) {
-        visited[v] = true;
-        int degree = this->neighbors[v].size();
-        for (int i = 0; i < degree; ++i) {
-            Vertex w;
-            neighbors[v].retrieve(i,w);
-            if (!visited[w]) {
-                recursive_depth_sort(w, visited, topological_order);
-            }
-        }
-        topological_order.insert(0, v);
-    }
+//    void recursive_depth_sort(Vertex v, bool visited[], list <Vertex> &topological_order) {
+//        visited[v] = true;
+//        int degree = this->neighbors[v].size();
+//        for (int i = 0; i < degree; ++i) {
+//            Vertex w;
+//            neighbors[v].retrieve(i,w);
+//            if (!visited[w]) {
+//                recursive_depth_sort(w, visited, topological_order);
+//            }
+//        }
+//        topological_order.insert(0, v);
+//    }
 };
 
 #endif //TEST_DIGRAPH_H
